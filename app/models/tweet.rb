4142f8eae2f074112451e5d8685fe7ca2220c6d9
PeerawasL <Peerawas.Likit@gmail.com>
2019-06-15 21:00:50 +0700

    Basic reply retweet like model and data association

---
+++ app/models/tweet.rb
@@ -0,0 +1,13 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+
+  has_many :likes
+  has_many  :liking_users, through: :likes, source: :user
+
+  belongs_to :parent, class_name: "Tweet", foreign_key: :parent_id, optional: true
+  has_many :replies, class_name: "Tweet", foreign_key: :parent_id
+  has_many :retweets, class_name: "Tweet", foreign_key: :parent_id
+
+  validates :content, presence: true
+
+end
---
+++ db/migrate/20190615103611_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.string :content
+      t.string :retweet_content
+
+      t.timestamps
+    end
+  end
+end
